afcc4cf1cd9d8efa5f10cf589166277a896511b1
tomas_vitale <tvitale@fi.uba.ar>
2026-05-20 04:17:07 +0000

    Corrección estetica del nombre de una funcion, deberia llamrse listar menu y se llamaba listar_usuarios

--- Backend/routes/menu.py
+++ Backend/routes/menu.py
@@ -4,7 +4,7 @@ from database.db import get_connection
 menu_bp = Blueprint("menu", __name__)
 
 @menu_bp.route("", methods = ["GET"])
-def listar_usuarios():
+def listar_menu():
     try:
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
