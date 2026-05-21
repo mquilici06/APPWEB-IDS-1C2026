67b8278d3b78c5d3b18932c2078ce5c882391773
mquilici <mquilici@fi.uba.ar>
2026-05-21 20:49:07 -0300

    Agrego endpoint get reservas, vista del cliente

--- Backend/routes/reservas.py
+++ Backend/routes/reservas.py
@@ -1,3 +1,45 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from database.db import get_connection
 
 reservas_bp = Blueprint("reservas", __name__)
+
+@reservas_bp.route("/disponibilidad", methods=["GET"])
+def consultar_disponibilidad():
+
+    fecha = request.args.get('fecha')
+    
+    if not fecha:
+        return jsonify({"error": "Falta el parámetro 'fecha'"}), 400
+
+    horarios_disp = ["20:00", "20:30", "21:00", "21:30", "22:00"]
+    capacidad_max = 10
+    
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+    except:
+        return jsonify({"Error": "Error de conexion con la base de datos"}),500
+    
+    cursor.execute("""SELECT hora, COUNT(*) as total
+                   FROM reservas WHERE fecha = %s AND estado = %s 
+                   GROUP BY hora""", (fecha, "confirmada"))
+    resultados = cursor.fetchall()
+
+    ocupacion = {}
+    for fila in resultados:
+        hora_limpia = str(fila['hora'])[:5]
+        ocupacion[hora_limpia] = fila['total']
+
+    disponibilidad = []
+    for h in horarios_disp:
+        cantidad = ocupacion.get(h, 0)
+        disponibilidad.append({
+            "hora": h,
+            "estado": "disponible" if cantidad < capacidad_max else "agotado",
+            "lugares_libres": capacidad_max - cantidad
+        })
+    
+    cursor.close()
+    conn.close()
+
+    return jsonify(disponibilidad), 200
