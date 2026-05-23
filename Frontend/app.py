14642621317da98d71978bfa7fa3eff400124f80
Juanpre-cyber <jxpresas@gmail.com>
2026-05-23 15:41:11 -0300

    borre el blueprint de menu, y cree el template para el error 404

--- Frontend/app.py
+++ Frontend/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 
 app.register_blueprint(mis_rutas, url_prefix="")
 
+@app.errorhandler(404)
+def pagina_no_encontrada(e):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
