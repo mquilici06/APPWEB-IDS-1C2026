a4d9c56d6fb726a24fc4c93044eff9d0f951fe8c
Juanpre-cyber <jxpresas@gmail.com>
2026-05-24 22:11:36 -0300

    ahora index es /, para que cuando pongmaos la ip entre directo al index

--- Frontend/routes/routes.py
+++ Frontend/routes/routes.py
@@ -6,7 +6,7 @@ mis_rutas = Blueprint('rutas_prefijo_api', __name__)
 def menu():
     return render_template("menu.html")
 
-@mis_rutas.route("/index")
+@mis_rutas.route("/")
 def index():
     return render_template("index.html")
 
