3465555ad05389aad877b9022d6bac96df0e875b
enzo <emiotti@fi.uba.ar>
2026-05-30 19:54:51 +0000

    agrego lo que faltaba al admin.css

--- Backend/database/db.py
+++ Backend/database/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root",
+        password="Root123!",
         database="altezza"
     )
