0af910ad50ecd733e8e02491e4c2d9ed2a2d4d1c
Julian Hidalgo <juhidalgo@fi.uba.ar>
2026-05-31 19:43:28 +0000

    comienzo con logica del contacto.py (sin terminar)

--- Frontend/routes/contacto.py
+++ Frontend/routes/contacto.py
@@ -1,1 +1,19 @@
+from flask import Blueprint, render_template, request
+
+contacto_bp = Blueprint("contacto", __name__)
+
+@contacto_bp.route("/contacto", methods=["GET", "POST"])
+def contactanos():
+
+    if request.method == "POST":
+
+        nombre = request.form.get("fnombre", "").strip()
+        email = request.form.get("femail", "").strip()
+        mensaje = request.files.get("fmensaje", "").strip()
+
+
+        return render_template("contacto.html")
+    
+    return render_template("contacto.html")
+#no esta terminado falta que agregue la logica del mail con flask-mail
 
