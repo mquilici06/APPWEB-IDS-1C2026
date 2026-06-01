0339594c48f7ad2db3f9b16f7892d6555b9fc02b
Julian Hidalgo <juhidalgo@fi.uba.ar>
2026-06-01 19:29:10 +0000

    Agrego en el app.py las configuraciones de flask_mail (no terminado, lo aclara el codigo) y en contacto.py agrego la logica de envio del mail y de los avisos de error o exito

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_mail import Mail
 from routes.routes import mis_rutas
 from routes.auth import auth_bp
 from routes.contacto import contacto_bp
@@ -9,6 +10,17 @@ app.secret_key = "altezza_password"
 app.register_blueprint(mis_rutas, url_prefix="")
 app.register_blueprint(auth_bp)
 app.register_blueprint(contacto_bp)
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = "altezzaadmin@gmail.com"
+app.config["MAIL_PASSWORD"] = "wvbktnqaujhizdji"  #hay q pasarlo a constante dsp seguro creo un .env para guardar datos mas seguro
+app.config["MAIL_DEFAULT_SENDER"] = "altezzaadmin@gmail.com"
+
+mail = Mail(app)
+
 
 @app.errorhandler(404)
 def pagina_no_encontrada(e):
--- Frontend/routes/contacto.py
+++ Frontend/routes/contacto.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
+from flask_mail import Message,Mail
 
 contacto_bp = Blueprint("contacto", __name__)
 
@@ -11,9 +12,24 @@ def contactanos():
         email = request.form.get("femail", "").strip()
         mensaje = request.form.get("fmensaje", "").strip()
 
+        if not nombre or not email or not mensaje:
+            return render_template("contacto.html", error="Por favor complete todos los campos")
+
         if "@" not in email:
-            return {"error": "Ingrese un correo electrónico válido"}, 400
-    
+            return render_template("contacto.html", error="Por favor ingrese un E-mail valido")
+        
+        mensaje_mail = Message(
+            subject="Nuevo mensaje de ALTEZZA-clientes",
+            recipients=["altezzaadmin@gmail.com"],
+            reply_to=email,
+        )   
+
+        mensaje_mail.body = render_template("e-mail_contacto.txt", nombre=nombre, email=email, mensaje=mensaje)
+        mail = Mail(current_app)
+        mail.send(mensaje_mail)
+        
+        return render_template("contacto.html",exito="Mensaje enviado correctamente")
+
     return render_template("contacto.html")
-#no esta terminado falta que agregue la logica del mail con flask-mail
+
 
