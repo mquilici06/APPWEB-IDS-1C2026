897a8f804a3c57e1d773cc0053ff093992df064d
tomas vitale <tvitale@fi.uba.ar>
2026-06-07 21:52:03 -0300

    No era necesario establecer el minimo ya que reservas.py ya lo validaba

--- Frontend/static/js/reservas.js
+++ Frontend/static/js/reservas.js
@@ -10,6 +10,3 @@ document.getElementById("fecha_reserva").addEventListener("input", function () {
 document.getElementById("notas").addEventListener("input", function () {
     document.getElementById("contador-notas").textContent = this.value.length + " / 300";
 });
- 
-const hoy = new Date().toISOString().split("T")[0];
-document.getElementById("fecha_reserva").min = hoy;
