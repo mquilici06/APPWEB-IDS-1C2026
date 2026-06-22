24ea57aa1b83f08eb3c8c9d9ff3224d9211f5128
Julian Hidalgo <juhidalgo@fi.uba.ar>
2026-06-22 19:21:47 +0000

    Agrego int al puerto en mail y validacion booleana en TLS SSL

--- Frontend/app.py
+++ Frontend/app.py
@@ -16,9 +16,9 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(contacto_bp)
 
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = os.getenv("MAIL_PORT")
-app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS")
-app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL")
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "False").lower() == "true"
+app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")  
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
